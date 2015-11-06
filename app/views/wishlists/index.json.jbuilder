8cc51ada0969a73cfff007ff6d87d7afe0166ddc
Wentao1026 <momi2020@uw.edu>
2015-11-05 21:01:18 -0500

    Wishlist application with some bugs...

---
+++ app/views/wishlists/index.json.jbuilder
@@ -0,0 +1,4 @@
+json.array!(@wishlists) do |wishlist|
+  json.extract! wishlist, :id, :user_id, :name
+  json.url wishlist_url(wishlist, format: :json)
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151015060558) do
+ActiveRecord::Schema.define(version: 20151027142110) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -51,5 +51,14 @@ ActiveRecord::Schema.define(version: 20151015060558) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "wishlists", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "wishlists", ["user_id"], name: "index_wishlists_on_user_id"
 
 end
