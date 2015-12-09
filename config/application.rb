58f50edafdd5d9d8d0f783f415eafba30dbf5da3
wugui2020 <momi2020@uw.edu>
2015-12-08 23:53:27 -0500

    apply 2

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -24,5 +26,6 @@ module OnlineShop
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths += %W(#{config.root}/lib)
+    config.active_job.queue_adapter = :delayed_job
   end
 end
